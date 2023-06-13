ec2035345401fe0bff6ee8dd0e09133f4f58430c
bardinGL <haonguyen20032003@gmail.com>
2023-06-13 09:59:35 +0700

    Add project files.

---
+++ schoolinformationportal/Validation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolinformationportal
+{
+    public class Validation
+
+    {
+        public String createRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1).ToString();
+            
+        }
+        public String getFirstLetter(string Description) {
+            string[] words = Description.Split(' ');
+            StringBuilder ID = new StringBuilder(); 
+            foreach (string word in words)
+            {
+                if(!string.IsNullOrEmpty(word))
+                {
+                    ID.Append(word[0]);
+                }
+            }
+            return ID.ToString();
+    }
+    public string createStudentID(String majorID, String studentName) { 
+            Validation validation = new Validation();
+
+            String studentID = "";
+            studentID = majorID + validation.getFirstLetter(studentName) ;
+            return studentID;
+        }
+        public string createEmail(string ID)
+        { Validation validation = new Validation();
+        String Email;
+        Email = ID + validation.createRandomNumber(1,1000000).PadLeft(6) + "@gmail.com" ;
+            return Email;
+        }
+        public string createclassID(String majorID) {
+            Validation validation = new Validation();
+            String classID = majorID + validation.createRandomNumber(1,10000).PadLeft(4) ;
+            return classID;
+        }
+        public string createMajorID(String majorDescription)
+        {
+            Validation validation = new Validation();
+            String majorID = validation.getFirstLetter(majorDescription) ;
+            return majorID;
+        }
+        public String createSubjectID(String SubjectDescrpition) {
+            Validation validation = new Validation();
+            String subjectID = validation.getFirstLetter(SubjectDescrpition) ;
+            return subjectID;
+        }
+        public String createAdminID(String Name) {
+        Validation validation = new Validation();
+            String adminID = "ad" + Name ;
+            return adminID;
+        }
+        public String createApplicationID()
+        {
+            Validation validation = new Validation();
+                return validation.createRandomNumber(1, 10000).PadLeft(4);
+            
+        }
+    }
+}
