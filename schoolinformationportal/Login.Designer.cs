3768117ae8a0341892500643c6685aeb229ee304
Minh Nguyen <minhnnse172310@fpt.edu.vn>
2023-06-19 22:16:44 +0700

    Minh Log in

---
+++ schoolinformationportal/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PE_PRN
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPassword = new TextBox();
+            lbPassword = new Label();
+            lbUsername = new Label();
+            txtUsername = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(281, 166);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(210, 27);
+            txtPassword.TabIndex = 7;
+            // 
+            // lbPassword
+            // 
+            lbPassword.AutoSize = true;
+            lbPassword.Location = new Point(167, 169);
+            lbPassword.Name = "lbPassword";
+            lbPassword.Size = new Size(70, 20);
+            lbPassword.TabIndex = 6;
+            lbPassword.Text = "Password";
+            // 
+            // lbUsername
+            // 
+            lbUsername.AutoSize = true;
+            lbUsername.Location = new Point(167, 92);
+            lbUsername.Name = "lbUsername";
+            lbUsername.Size = new Size(75, 20);
+            lbUsername.TabIndex = 5;
+            lbUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(281, 89);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(210, 27);
+            txtUsername.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(584, 129);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 8;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(lbPassword);
+            Controls.Add(lbUsername);
+            Controls.Add(txtUsername);
+            Name = "Login";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPassword;
+        private Label lbPassword;
+        private Label lbUsername;
+        private TextBox txtUsername;
+        private Button btnLogin;
+    }
+}
