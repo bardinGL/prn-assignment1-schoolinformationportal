b74c54af678c67ede0866b423770009f655baee1
bardinGL <haonguyen20032003@gmail.com>
2023-06-20 09:57:06 +0700

    Hao commit

--- schoolinformationportal/StudentInformation.cs
+++ schoolinformationportal/StudentInformation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,15 +15,43 @@ namespace schoolinformationportal
 {
     public partial class StudentInformation : Form
     {
-        List<Student> StudentList;
+
+        List<Student> StudentList = new List<Student>();
+        StudentRepository StudentRepo = new StudentRepository();  
+
         
-        public StudentInformation(String getClassIDtocheck)
+        public StudentInformation(String getClassIDtocheck, String getEmailIDtocheck)
         {
             InitializeComponent();
-            txtStudentID.Text = getClassIDtocheck;
-       StudentInfoContext _context = new StudentInfoContext();
-            var studentList = _context.Students.Where(Student => Student.StudentId.Contains(getClassIDtocheck));
-            dataGridView1.DataSource = new BindingSource() { DataSource = StudentList};
+            StudentList = StudentRepo.GetAll();
+            //txtStudentID.Text = getClassIDtocheck;
+            StudentInfoContext _context = new StudentInfoContext();
+            var studentList = _context.Students.Where(Student => Student.ClassId.Contains(getClassIDtocheck)).ToList();
+            dataGridView1.DataSource = new BindingSource() { DataSource = studentList};
+
+            //foreach (var student in studentList)
+            //{
+            //    if (student.StudentEmail == getEmailIDtocheck)
+            //    {
+            //        txtStudentID.Text = student.StudentId.ToString();
+            //        txtStudentName.Text = student.StudentName.ToString();
+            //        txtStudentAge.Text = student.StudentAge.ToString();
+            //        txtStudentEmail.Text = getEmailIDtocheck;
+            //        txtStudentClass.Text = getClassIDtocheck;
+            //        break;
+            //    }
+            //}
+
+            var studentID = _context.Students.Where(Student => Student.StudentEmail == getEmailIDtocheck).Select(s => s.StudentId).FirstOrDefault();
+            var studentName = _context.Students.Where(Student => Student.StudentEmail.Contains(getEmailIDtocheck)).Select(s => s.StudentName).FirstOrDefault();
+            var studentAge = _context.Students.Where(Student => Student.StudentEmail.Contains(getEmailIDtocheck)).Select(s => s.StudentAge).FirstOrDefault();
+            txtStudentID.Text = studentID;
+            txtStudentName.Text = studentName;
+            txtStudentAge.Text = studentAge.ToString();
+            txtStudentClass.Text = getClassIDtocheck;
+            txtStudentEmail.Text = getEmailIDtocheck;
+
+
         }
 
         private void StudentInformation_Load(object sender, EventArgs e)
