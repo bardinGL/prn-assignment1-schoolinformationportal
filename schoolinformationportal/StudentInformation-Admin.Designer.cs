2e627ee61e5d65a07b68d3ed140242cf4be9a408
nghiantrong <nguyentrongnghia2003@gmail.com>
2023-06-23 10:37:26 +0700

    doing add function

---
+++ schoolinformationportal/StudentInformation-Admin.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace schoolinformationportal
+{
+    partial class StudentInformation_Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvStudentAdmin = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMajorID = new System.Windows.Forms.TextBox();
+            this.txtClassID = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSearchID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSearchReset = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentAdmin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudentAdmin
+            // 
+            this.dgvStudentAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentAdmin.Location = new System.Drawing.Point(31, 36);
+            this.dgvStudentAdmin.Name = "dgvStudentAdmin";
+            this.dgvStudentAdmin.RowTemplate.Height = 25;
+            this.dgvStudentAdmin.Size = new System.Drawing.Size(657, 229);
+            this.dgvStudentAdmin.TabIndex = 0;
+            this.dgvStudentAdmin.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudentAdmin_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(716, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "StudentID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(705, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(716, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Student Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(708, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Student Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(722, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "MajorID";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(726, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ClassID";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Enabled = false;
+            this.txtStudentID.Location = new System.Drawing.Point(836, 36);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(163, 23);
+            this.txtStudentID.TabIndex = 7;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(836, 97);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(163, 23);
+            this.txtStudentName.TabIndex = 8;
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(836, 154);
+            this.nudAge.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudAge.Minimum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(58, 23);
+            this.nudAge.TabIndex = 9;
+            this.nudAge.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(836, 213);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(163, 23);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // txtMajorID
+            // 
+            this.txtMajorID.Location = new System.Drawing.Point(836, 275);
+            this.txtMajorID.Name = "txtMajorID";
+            this.txtMajorID.Size = new System.Drawing.Size(163, 23);
+            this.txtMajorID.TabIndex = 11;
+            // 
+            // txtClassID
+            // 
+            this.txtClassID.Location = new System.Drawing.Point(836, 324);
+            this.txtClassID.Name = "txtClassID";
+            this.txtClassID.Size = new System.Drawing.Size(163, 23);
+            this.txtClassID.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(934, 126);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(934, 156);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(31, 301);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Search StudentID";
+            // 
+            // txtSearchID
+            // 
+            this.txtSearchID.Location = new System.Drawing.Point(222, 303);
+            this.txtSearchID.Name = "txtSearchID";
+            this.txtSearchID.Size = new System.Drawing.Size(163, 23);
+            this.txtSearchID.TabIndex = 16;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(416, 303);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 17;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSearchReset
+            // 
+            this.btnSearchReset.Location = new System.Drawing.Point(511, 303);
+            this.btnSearchReset.Name = "btnSearchReset";
+            this.btnSearchReset.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchReset.TabIndex = 18;
+            this.btnSearchReset.Text = "Reset";
+            this.btnSearchReset.UseVisualStyleBackColor = true;
+            this.btnSearchReset.Click += new System.EventHandler(this.btnSearchReset_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(934, 185);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // StudentInformation_Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1034, 361);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearchReset);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtClassID);
+            this.Controls.Add(this.txtMajorID);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvStudentAdmin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StudentInformation_Admin";
+            this.Text = "StudentInformation_Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentAdmin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgvStudentAdmin;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtStudentID;
+        private TextBox txtStudentName;
+        private NumericUpDown nudAge;
+        private TextBox txtEmail;
+        private TextBox txtMajorID;
+        private TextBox txtClassID;
+        private Button btnAdd;
+        private Button btnReset;
+        private Label label7;
+        private TextBox txtSearchID;
+        private Button btnSearch;
+        private Button btnSearchReset;
+        private Button btnDelete;
+    }
+}
---
+++ schoolinformationportal/StudentInformation-Admin.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Service.Models;
+using Service.Repository;
+ 
+
+namespace schoolinformationportal
+{
+    public partial class StudentInformation_Admin : Form
+    {
+        StudentRepository studentRepository = new StudentRepository();
+        List<Student> _studentList = new List<Student>();
+        StudentInfoContext _studentInfoContext = new StudentInfoContext();
+
+        public StudentInformation_Admin()
+        {
+            InitializeComponent();
+            _studentList = studentRepository.GetAll();
+            dgvStudentAdmin.DataSource = new BindingSource() { DataSource = _studentList};
+
+            // Hide the 'Class' column
+            dgvStudentAdmin.Columns["Class"].Visible = false;
+
+            // Hide the 'Major' column
+            dgvStudentAdmin.Columns["Major"].Visible = false;
+
+            // Hide the 'StudentEmailNavigation' column
+            dgvStudentAdmin.Columns["StudentEmailNavigation"].Visible = false;
+
+            // Hide the 'Applications' column
+            dgvStudentAdmin.Columns["Applications"].Visible = false;
+
+        }
+
+        private void dgvStudentAdmin_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var student = _studentList[e.RowIndex];
+            txtStudentID.Text = student.StudentId.ToString();
+            txtStudentName.Text = student.StudentName.ToString();
+            nudAge.Value = student.StudentAge;
+            txtEmail.Text = student.StudentEmail.ToString();
+            txtMajorID.Text = student.MajorId.ToString();
+            txtClassID.Text = student.ClassId.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtStudentID.Text = "";
+            txtStudentName.Text = "";
+            nudAge.Value = 18;
+            txtEmail.Text = "";
+            txtMajorID.Text = "";
+            txtClassID.Text = "";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            _studentInfoContext = new StudentInfoContext();
+            var searchValue = txtSearchID.Text;
+            var list = _studentInfoContext.Students.Where(student => student.StudentId.Equals(searchValue)).ToList();
+            
+            if (list.Count > 0)
+            {
+                dgvStudentAdmin.DataSource = new BindingSource() { DataSource = list };
+            }
+            else
+            {
+                MessageBox.Show("Student Not Found!", "Notification", MessageBoxButtons.OK);
+                var studentAllList = studentRepository.GetAll();
+                dgvStudentAdmin.DataSource = new BindingSource() { DataSource = studentAllList };
+            }
+            
+
+        }
+
+        private void btnSearchReset_Click(object sender, EventArgs e)
+        {
+            var studentAllList = studentRepository.GetAll();
+            dgvStudentAdmin.DataSource = new BindingSource() { DataSource = studentAllList };
+            txtSearchID.Text = "";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Validation validation = new Validation();
+                string studentName = txtStudentName.Text;
+                string studenId = validation.createStudentID("SE", studentName);
+                //string studenId = "SE69";
+
+                int studentAge = (int) nudAge.Value;
+                string email = txtEmail.Text;
+                string majorId = txtMajorID.Text;
+                string classId = txtClassID.Text;
+
+                Student student = new Student
+                {
+                    StudentId = studenId,
+                    StudentName = studentName,
+                    StudentAge = studentAge,
+                    StudentEmail = email,
+                    MajorId = majorId,
+                    ClassId = classId,
+                };
+
+                //student.StudentId = studenId;
+                //student.StudentName = studentName;
+                //student.StudentAge = studentAge;
+                //student.StudentEmail = email;
+                //student.MajorId = majorId;
+                //student.ClassId = classId;
+
+                //foreach (Student stu in _studentList)
+                //{
+                //    if (stu.StudentId.Equals(student.StudentId))
+                //    {
+
+                //    }
+                //}
+                StudentRepository _studentRepository = new StudentRepository();
+                _studentRepository.Create(student);
+                dgvStudentAdmin.DataSource = new BindingSource() { DataSource = _studentRepository.GetAll() };
+
+                txtStudentID.Text = "";
+                txtStudentName.Text = "";
+                nudAge.Value = 18;
+                txtEmail.Text = "";
+                txtMajorID.Text = "";
+                txtClassID.Text = "";
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                MessageBox.Show(message, "Notification", MessageBoxButtons.OK);
+                Console.WriteLine(message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to remove this student?", "Remove Student", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string deletedId = txtStudentID.Text;
+                studentRepository.Delete(_studentList.FirstOrDefault(x => x.StudentId.Equals(deletedId)));
+                _studentList = studentRepository.GetAll();
+                dgvStudentAdmin.DataSource = new BindingSource() { DataSource = _studentList };
+
+                txtStudentID.Text = "";
+                txtStudentName.Text = "";
+                nudAge.Value = 18;
+                txtEmail.Text = "";
+                txtMajorID.Text = "";
+                txtClassID.Text = "";
+            }
+        }
+    }
+}
